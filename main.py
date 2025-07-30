33878f5c6000737af771f0b81c585086199c36eb
David Mkhitaryan <dmkhitaryan@github.com>
2025-07-30 19:19:14 +0200

    More conversion options + negating pixels

--- main.py
+++ main.py
@@ -7,11 +7,24 @@ img = Image.open("./Regulus_Icon.jpg")
 print(f"Successfully loaded image!\nImage size: {img.width} x {img.height}")
 
 def rgb_to_brightness(data, transform_type):
+    #print(data)
     match transform_type:
         case "average":
             return [math.floor(sum(item) / 3) for item in data]
+        case "luminance":
+            return [math.floor(0.2126*item[0] + 0.7152*item[1] + 0.0722*item[2]) for item in data]
+        case "desaturation":
+            return [math.floor( (max(item) + min(item)) / 2 ) for item in data]
+        case "decomposition_min":
+            return [math.floor(min(item)) for item in data]
+        case "decomposition_max":
+            return [math.floor(max(item)) for item in data]
         case _:
             print("Nothing!")
+            
+def negative_filter(data):
+    #print([tuple(map(lambda x: abs(x - 255), item)) for item in data])
+    return [tuple(map(lambda x: abs(x - 255), item)) for item in data]
 
 def print_ascii_chars(data):
     ascii_chars = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
@@ -26,5 +39,8 @@ def print_ascii_chars(data):
     
                     
 data = [img.getpixel((x,y)) for y in range(img.height) for x in range(img.width)]
+#data = rgb_to_brightness(data, "average")
+#print_ascii_chars(data)
+data = negative_filter(data)
 data = rgb_to_brightness(data, "average")
 print_ascii_chars(data)
